0e28a62e07315f94aeb2a7bed5789c00783921b0
Jonathan Tsang <jw.tsang@gmail.com>
2022-07-22 19:28:22 -0300

    feat(telus): add page & route

---
+++ src/organisms/TelusChallenge/TelusChallenge.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import AppContainer from "../../templates/AppContainer";
+import H1 from "atoms/H1";
+
+export const TelusChallenge: React.VFC = () => {
+
+  return (
+    <AppContainer>
+      <H1>Hello, TELUS</H1>
+    </AppContainer>
+  );
+};
+
+export default TelusChallenge;
---
+++ src/pages/Telus.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react';
+import { TelusChallenge } from '../organisms/TelusChallenge/TelusChallenge';
+
+export const Telus: React.VFC = () => <TelusChallenge />;
