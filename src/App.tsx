7fa2a8741c5e41ff95e512cd37cab066f1b94df8
Jonathan Tsang <jw.tsang@gmail.com>
2022-07-25 01:34:19 -0300

    fix(app): remove exact path

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export const App: React.VFC = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/telus" component={Telus} />
+      <Route path="/telus" component={Telus} />
     </Switch>
   );
 };
