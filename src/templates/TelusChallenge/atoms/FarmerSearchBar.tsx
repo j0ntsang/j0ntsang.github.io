1347de0846451847000f6b36fabbce4ba26a5eb8
Jonathan Tsang <jw.tsang@gmail.com>
2022-07-25 01:44:17 -0300

    fix(farmer-search-bar): increase font visibility

--- src/templates/TelusChallenge/atoms/FarmerSearchBar.tsx
+++ src/templates/TelusChallenge/atoms/FarmerSearchBar.tsx
@@ -17,7 +17,7 @@ export const FarmerSearchBar = ({ handleChange }: Props) => {
         <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
           <svg aria-hidden="true" className="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
         </div>
-        <input onChange={(e) => handleChange(e.target.value) } type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Name, City" />
+        <input onChange={(e) => handleChange(e.target.value) } type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-white bg-gray-50 rounded-lg border bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Name, City" />
         <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-4 py-2">Search</button>
       </div>
     </FarmerSearchBarWrapper>
