dd0279887541784721577f92a3eff3ee692e3946
Jonathan Tsang <jw.tsang@gmail.com>
2025-03-03 00:26:35 -0400

    add doc title animation demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
 
+import React, { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import { Home } from "./pages/Home";
-import React from "react";
 import { Test } from "./pages/Test";
+import useTitleAnimation from "./hooks/useTitleAnimation";
 
 export const App: React.VFC = () => {
+  const [animationType] = useState("progress");
+
+  useTitleAnimation(animationType);
+
   return (
     <Switch>
       <Route exact path="/" component={Home} />
