4d74cde4aab8de23ac5017423caa25cf7860ba83
Jonathan Tsang <jw.tsang@gmail.com>
2025-06-03 11:21:13 -0300

    remove page title animation, update test import

--- src/react/App.test.tsx
+++ src/react/App.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 
-import { App } from "./react/App";
+import { App } from "./App";
 
 test("renders the talent name", () => {
   render(<App />);
--- src/react/App.tsx
+++ src/react/App.tsx
@@ -2,10 +2,7 @@ import "./App.css";
 
 import { Home } from "./pages/Home";
 import React from "react";
-import useTitleAnimation from "./hooks/useTitleAnimation";
 
 export const App: React.VFC = () => {
-  useTitleAnimation("hash_dot");
-
   return <Home />;
 };
